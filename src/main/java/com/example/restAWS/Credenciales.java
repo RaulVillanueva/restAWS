360238e864d0d3b07834bde01ed974efb1cdb978
Raúl Villanueva <a16001061@alumnos.uady.mx>
2023-12-12 18:58:12 -0600

    Credenciales final

--- src/main/java/com/example/restAWS/Credenciales.java
+++ src/main/java/com/example/restAWS/Credenciales.java
@@ -1,8 +1,8 @@
 package com.example.restAWS;
 
 public class Credenciales {
-    public static final String ACCESS_KEY = "ASIA6CRJ335EK663RBW6";
-    public static final String SECRET_KEY = "ZpxYwogeeBBnZWivo5l0lQe7QdP4oHZSGNcCDAE2";
-    public static final String SESSION_TOKEN = "FwoGZXIvYXdzELn//////////wEaDDGvvMnvH0h+Y/Fa2yLUAWxh4uapO6/rq9SS2kHkkAkJ1uf+UZL1Agbc87hknn8d4IZzyHDPxFkqSs2dSV6oZtRQibB533alFXSvCoI+DE06hLXOhYMw6Y/nKYt97MZ9qgAnr+ERPay26tl5T8hTngJpoWDx9hWlUG93UJICnexsY2KfXVLl2Qc4RpJCmkgx6aXMwPOR4oWnacm0kogYWMRN5YCL1PC5JjgZAb8LrN1X3DSK3rJjT/2qDnBlxB+BrOpdPd9v0OAHRepe2qaFst+emoZlfADuot5CJ0mUxIvToDTuKNXC3qsGMi2K9hdrpOShLrhOfkh8k0nwnvaDvR+FFTqs8IICN/vgEpdYkIAIziau8qDG++Q=";
+    public static final String ACCESS_KEY = "ASIA6CRJ335EMVT3N5U4";
+    public static final String SECRET_KEY = "RRKBCTEneRExhGXzxe1Nj+91DgIcrlucn1OZRJfV";
+    public static final String SESSION_TOKEN = "FwoGZXIvYXdzENL//////////wEaDMgngbaP3PqNKDa70iLUAfFos5Yc7ckuyqgbhyOPU/yI3f3JdznTOn4eRmTzVSg7kf5lV+Yb1vDYq7WXLxkLdpc7zlSU7U4rQGOPsCBN8Q1SFYXcuBu6IWfVgB7GFW2zGEO0pCYGSzC39GK5KdKfq4lgQid/lSsj1lDvtBQ+Xznzcf8p6vhUPsw+JxwjlE0xERNvjC3tKelYfCwG9pga7b7da8ydGdljXuZVwYeuZgE8OBgh//HmeCVEWgN19jYCPsfkDPIIvNcrw8DuHAean/VTM+RjSFJMp0PIt4gmwolU45l2KKaC5KsGMi3EBe7NXfVCoBzm4QM0e59F/yArnG8W9EtDZp5b5Ov4biaqtPVMQ6c/bYRAOqI=";
     
 }
